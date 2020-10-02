dc7976ec3ae9d33980388f4ae700732899d8e1a4
Yeferson <yaguilar@unal.edu.co>
2020-10-02 17:53:09 -0500

    usuario

--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,2 +1,15 @@
 public class Usuario {
+    public int documento;
+    public String nombre;
+    public String apellido;
+    public String correo;
+    public String contraseña;
+    public Usuario(int documento, String nombre, String apellido, String correo, String contraseña){
+        this.documento=documento;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.correo=correo;
+        this.contraseña=contraseña;
+    }
 }
+ //Documento, Nombre, Apellido, Correo y Contraseña
