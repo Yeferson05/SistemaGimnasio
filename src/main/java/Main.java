f99a9dbd45d41ee72a23f1aa536902ecadc71533
Yeferson <yaguilar@unal.edu.co>
2020-10-02 18:00:36 -0500

    Autenticación

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        String option;
+        while(true) {
+            System.out.println();
+            System.out.println("-----------------------");
+            System.out.println("Bienvenido");
+            System.out.println("Escoja una opcion:");
+            System.out.println("1. Ingresar");
+            System.out.println("2. Registrarse");
+            System.out.println("0. Salir");
+            System.out.println();
+            option = input.next();
+            if (option.equals("1")) {
+                //ingresar();
+            } else if (option.equals("2")) {
+                // registrarse();
+            } if(option.equals("0")){
+                break;
+            }
+        }
+    }
 }
