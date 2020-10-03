3861537ffead1f3cd8cd4587adb2db78d1208268
Kevin Quiñones <kquinones107@unal.edu.co>
2020-10-03 02:25:30 -0500

    Clase Sede

--- src/main/java/Sede.java
+++ src/main/java/Sede.java
@@ -1,2 +1,11 @@
 public class Sede {
+    public String nombreSede;
+    public String direccion;
+    public String ciudad;
+
+    public Sede(String nombreSede, String direccion, String ciudad) {
+        this.nombreSede = nombreSede;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+    }
 }
