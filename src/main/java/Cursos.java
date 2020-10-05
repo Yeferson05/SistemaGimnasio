b1403b7216d4f38575a9c23f0f776067b9074eee
Kevin Quiñones <kquinones107@unal.edu.co>
2020-10-04 22:54:05 -0500

    curso

--- src/main/java/Cursos.java
+++ src/main/java/Cursos.java
@@ -1,2 +1,21 @@
 public class Cursos {
+
+    public String nombre;
+    public int codigo;
+    public int intesidadHora;
+
+    public Cursos(String nombre, int codigo, int intesidadHora) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.intesidadHora = intesidadHora;
+    }
+
+    @Override
+    public String toString() {
+        return "Cursos{" +
+                "nombre='" + nombre + '\'' +
+                ", codigo=" + codigo +
+                ", intesidadHora=" + intesidadHora +
+                '}';
+    }
 }
